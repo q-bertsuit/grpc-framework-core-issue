e684c68879cee115dff90f542ae1199dce16d3f0
Magnus Dale <md@sekal.com>
2020-09-18 11:02:22 +0200

    first commit

---
+++ Test_gRPC.NetFrameworkClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using TesGrpc;
+namespace Test_gRPC.NetFrameworkClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Channel channel = new Channel("127.0.0.1:5001", ChannelCredentials.Insecure);
+            Channel channel = new Channel("localhost", 5001, ChannelCredentials.Insecure);
+
+            var    client = new Greeter.GreeterClient(channel);
+            String user   = "mags";
+
+            var reply = client.SayHello(new HelloRequest { Name = user });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test_gRPC.Protos/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Test_gRPC.Protos
+{
+    public class Class1
+    {
+    }
+}
